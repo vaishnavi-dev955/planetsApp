b9ef7351b365fff244a5dade7e38c0c18bc6950e
vaishnavi-dev955 <vaishnavimandala1998@gmail.com>
2023-03-07 18:19:20 +0000

    commit message

---
+++ src/components/PlanetItem/index.js
@@ -0,0 +1,16 @@
+import './index.css'
+
+const PlanetItem = props => {
+  const {item} = props
+  const {name, description, imageUrl} = item
+
+  return (
+    <div className="planet-container">
+      <img src={imageUrl} alt={`planet ${name}`} className="image" />
+      <h1 className="heading">{name}</h1>
+      <p className="description">{description}</p>
+    </div>
+  )
+}
+
+export default PlanetItem
